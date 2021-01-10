cf75ad851af8b9993de740119e7ce507e8c25f9c
ikozul00 <ivana.kozul5@gmail.com>
2021-01-10 15:16:59 +0100

    Changing file name

--- controllers.js
+++ controllers.js
@@ -3,7 +3,7 @@ const client = require('./conectingDatabase');
 const script=require("./src/functions.js");
 exports.readData = function(request, response) {
     response.writeHead(200, { 'Content-Type': 'text/html'});
-    fs.readFile('./index.html', null, function (error, data) {
+    fs.readFile('./src/testiranje.html', null, function (error, data) {
         if (error) {
             response.writeHead(404);
              respone.write('Whoops! File not found!');
