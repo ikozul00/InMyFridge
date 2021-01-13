e23af34cd9a8421c906b82438b829b2b7b32e6cb
imadunic <imadun00@fesb.hr>
2021-01-13 22:08:14 +0100

    Registracija v.2 - dodana provjera postoji li korisnik vec u bazi

--- controllerRegistration.js
+++ controllerRegistration.js
@@ -3,6 +3,7 @@ const fs=require("fs");
 const client = require('./conectingDatabase');
 //const funkcije=require("./registration.js");
 
+
 exports.SignUp=function(request,response){
 
   var username = request.body.username;
@@ -10,19 +11,42 @@ exports.SignUp=function(request,response){
     var surname = request.body.surname;
     var name = request.body.name;
 	var email = request.body.email;
+	var passwordRepeat = request.body.pswRepeat;
 
 	var sqlQuery = `INSERT INTO korisnik (ime, prezime, username, email, lozinka) VALUES ('${name}', '${surname}', '${username}', '${email}', '${password}')`;
+	var checkIfExists = `SELECT * FROM korisnik WHERE username='${username}' OR email = '${email}'`;
 
-	if (name && surname && email && username && password) {
-		client.query(sqlQuery, function (err, result) {
-
-				if (err)
-					throw err;
-
-				response.redirect("/");
-		
-			});
+	//provjeri je li lozinka oba puta dobro unesena
+	if(password != passwordRepeat){
+		response.status(404).json({message:'Password unmatched!'});
+		response.end();
 	}
+
+	else if (name && surname && email && username && password) {
+
+		//Provjeri postoji li vec takav user
+		client.query(checkIfExists, function(err, result){
+			if (err)
+					throw err;
+			if(typeof (result.rows[0]) !== "undefined"){
+				response.status(404).json({message:'User already exists!'});
+				response.end();
+			}
+
+			//Ako korisnik ne postoji u bazi, dodaj ga
+			else{	
+					client.query(sqlQuery, function (err, result) {
+
+						if (err)
+							throw err;
+
+						response.redirect("/");
+						response.end();
+					});
+			}
+		});
+	}
+
 	else{
 		response.status(404).json({message:'Please enter your infromation!'});
 		response.end();
