e053db778c45a7899e0ebba8653c94ea4a0a66db
Iva Madunic <iva.madunic.00@fesb.hr>
2021-01-13 17:48:41 +0100

    Registracija - uspjesna verzija 1

--- controllerRegistration.js
+++ controllerRegistration.js
@@ -9,24 +9,19 @@ exports.SignUp=function(request,response){
     var password = request.body.psw;
     var surname = request.body.surname;
     var name = request.body.name;
-    var email = request.body.email;
+	var email = request.body.email;
 
-    var sql = 'INSERT INTO korisnik (ime, prezime, username, email, lozinka) VALUES (?, ?, ?, ?, ?)';
-    var userInfo = [name, surname, username, email, password];
+	var sqlQuery = `INSERT INTO korisnik (ime, prezime, username, email, lozinka) VALUES ('${name}', '${surname}', '${username}', '${email}', '${password}')`;
 
 	if (name && surname && email && username && password) {
-		client.query(sql, userInfo, function(err, result) {
-            
-            if (err) throw err;
+		client.query(sqlQuery, function (err, result) {
 
-			if (result.affectedRows) {
+				if (err)
+					throw err;
+
 				response.redirect("/");
-			}
-			else {
-				response.status(404).json({message:'Incorrect Username and/or Password!'});
-			}			
-			response.end();
-		});
+		
+			});
 	}
 	else{
 		response.status(404).json({message:'Please enter your infromation!'});
